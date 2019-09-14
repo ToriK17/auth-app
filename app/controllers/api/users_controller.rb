395082c7f37a2e121bf74c5ce6376a331780ab0a
ToriK17 <vfcad@protonmail.com>
2019-09-14 14:47:58 -0700

    initial commit

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+  def show
+    if current_user
+      render json: {name: current_user.name}
+    else
+      render json: [], status: :unauthorized
+    end 
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user.save
+      render json: {message: 'User created successfully'}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
+
+end
